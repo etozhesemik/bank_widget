f9be54b028251e9d7f94e4321b729695a76fab3e
Semen Maslov <semen.masloff@gmail.com>
2025-01-24 17:00:49 +0300

    function sort_by_date added

--- src/processing.py
+++ src/processing.py
@@ -9,3 +9,7 @@ def filter_by_state(lists: Union[list], user_state="EXECUTED") -> list:
         if search_state == user_state:
             new_list.append(list)
     return new_list
+
+
+def sort_by_date(lists: Union[list], reverse: Union[bool]=False) -> list:
+    return sorted(lists, key = lambda x: x["date"], reverse=reverse)
