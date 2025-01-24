7f4342cc388c3bfc21ec8eacffb40742616e944b
Semen Maslov <semen.masloff@gmail.com>
2025-01-24 17:11:00 +0300

    annotations fixes

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,9 @@
 from typing import Union
 
 
-def filter_by_state(lists: Union[list], user_state="EXECUTED") -> list:
+def filter_by_state(lists: Union[list], user_state: Union[str] = "EXECUTED") -> list:
+    """Функция сортирует список по параметру 'state'"""
+
     new_list = []
 
     for list in lists:
@@ -11,5 +13,7 @@ def filter_by_state(lists: Union[list], user_state="EXECUTED") -> list:
     return new_list
 
 
-def sort_by_date(lists: Union[list], reverse: Union[bool]=False) -> list:
-    return sorted(lists, key = lambda x: x["date"], reverse=reverse)
+def sort_by_date(lists: Union[list], reverse: Union[bool] = False) -> list:
+    """Функция сортирует список по дате"""
+
+    return sorted(lists, key=lambda x: x["date"], reverse=reverse)
